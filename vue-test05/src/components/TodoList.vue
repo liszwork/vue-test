d2a321c17bd2aa8f73ffeb440d48f88e0c955ad2
lisns <z.z.iz.0pnup@gmail.com>
2020-03-24 13:16:49 +0900

    LocalStorageへのSave/Load、ref memo

--- vue-test05/src/components/TodoList.vue
+++ vue-test05/src/components/TodoList.vue
@@ -1,5 +1,9 @@
-// Todo List refs.
-// https://qiita.com/moonglows76/items/358ef3cd1566c38ece3a
+/*-------------------------------------------------------
+Todo List refs.
+https://qiita.com/moonglows76/items/358ef3cd1566c38ece3a
+Browser local storage check
+https://qiita.com/k_yumoto/items/d2026128a08ed8fab22c
+-------------------------------------------------------*/
 <template>
   <div class="content">
     <h1>Todo list</h1>
@@ -9,14 +13,12 @@
       <button @click="onAdd">add</button>
     </div>
 
-
-
-
     <div id="todoContents">
       <ul>
-        <li class="todoContent" v-for="(todo, index) in todos" :key="todos.content">
+        <li class="todoContent" v-for="(todo, index) in todos" :key="todos.id">
           <label v-bind:class="{ done: todo.isChecked }">
-            <input type="checkbox" name="" id="" v-model="todo.isChecked">{{ todo.content }}
+            <input type="checkbox" name id v-model="todo.isChecked" />
+            {{ todo.content }}
           </label>
           <button @click="onDelete">delete</button>
         </li>
@@ -33,9 +35,7 @@ export default {
       chk: false,
       addText: "",
       lastId: 1,
-      todos: [
-        { id:0, content:"test", isChecked: true }
-      ]
+      todos: [{ id: 0, content: "test", isChecked: true }]
     };
   },
   methods: {
@@ -43,7 +43,7 @@ export default {
       let item = {
         id: this.lastId,
         content: this.addText,
-        isChecked: false,
+        isChecked: false
       };
       this.todos.push(item);
     },
@@ -60,35 +60,34 @@ export default {
     onDelete(event) {
       console.log("onDelete");
       // クリックされたオブジェクトを操作
-      this.item = this.todos.filter(function (item) {
+      this.item = this.todos.filter(function(item) {
         console.log(item.content);
         return item.isChecked === false;
       });
       this.save();
     },
     save() {
-      localStorage.setItem('todos', JSON.stringify(this.todos));
+      localStorage.setItem("todos", JSON.stringify(this.todos));
     },
     load() {
-      console.log('load');
-      this.todos = JSON.parse(localStorage.getItem('todos'));
+      console.log("load");
+      this.todos = JSON.parse(localStorage.getItem("todos"));
       if (!this.todos) {
-      console.log(' No data');
+        console.log(" No data");
         this.todos = [];
-      }
-      else {
+      } else {
         let i = this.todos.length - 1;
-        this.lastId = thiss.todos[i].id;
-      console.log(' last id ' + this.lastId);
+        this.lastId = this.todos[i].id;
+        console.log(" last id " + this.lastId);
       }
-    },
-    created: function() {
-      console.log('created');
-    },
-    mounted: function () {
-      console.log('mounted');
-      this.load();
     }
+  },
+  created: function() {
+    console.log("created");
+  },
+  mounted: function() {
+    console.log("mounted");
+    this.load();
   }
 };
 </script>
